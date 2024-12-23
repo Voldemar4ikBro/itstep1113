044545d4ef6c42e5fcffad1562c64e533e8b4267
Voldemar4ikBro <vovkasyhsko@gmail.com>
2024-12-23 15:51:50 +0200

    Student

--- OOP/student.py
+++ OOP/student.py
@@ -1,63 +1,78 @@
-#симулятор життя студента
 import random
 
 class Student:
     def __init__(self, name):
-        self.name=name
-        self.progress=0 #100
-        self.lvlife=True
-        self.mood=50 #100
+        self.name = name
+        self.progress = 0
+        self.lvlife = True
+        self.mood = 50
+        self.money = 100
 
     # методи класу
     def study(self):
         print("Час для навчання")
-        self.progress+=random.randint(3,10)
-        self.mood-=random.randint(1,7)
+        self.progress += random.randint(3, 10)
+        self.mood -= random.randint(1, 7)
+        self.money -= random.randint(5, 10)  # витрати під час навчання
+
     def sleep(self):
         print("Час для сну")
-        self.mood -= random.randint(1, 7)
+        self.mood += random.randint(3, 10)
+
     def rest(self):
         print("Час для відпочинку")
         self.progress -= random.randint(1, 7)
         self.mood += random.randint(3, 10)
+        self.money -= random.randint(10, 20)  # витрати під час відпочинку
+
+    def work(self):
+        print("Час для роботи")
+        self.money += random.randint(20, 50)
+        self.mood -= random.randint(3, 7)
+        self.progress -= random.randint(1, 5)
+
     def isLvlife(self):
-        if self.progress<4:
+        if self.progress < 4:
             print("Ймовірність виключення із закладу")
-            self.lvlife=False
-        elif self.progress<9:
-            print("Підготовка до сесії\nБезсонні ночі")
-            self.lvlife=False
-        elif self.progress<13:
-            print("Екзамен екстерном \nЗбільшення часу на відпочинок")
+            self.lvlife = False
+        elif self.mood < 10:
+            print("Студент в депресії")
+            self.lvlife = False
+        elif self.money < 0:
+            print("Немає грошей")
             self.lvlife = False
 
     def everyday(self):
-        print("\033[33mУспішність: ", self.progress, "\nНастрій: ", self.mood,"\033[0m")
-    def lifeStud(self, day):
-        day="\033[34mдень №"+str(day)+"\033[0m"
-        rnd=random.randint(1,3)
-        print(day)
-        if rnd==1:
-            self.study()
-        elif rnd==2:
-            self.sleep()
-        elif rnd==3:
-            self.rest()
-            self.everyday()
-            self.isLvlife()
+        print("\033[33mУспішність: ", self.progress, "\nНастрій: ", self.mood, "\nГроші: ", self.money, "\033[0m")
 
-st1=Student("Сашко")
-#print(st1.name, "Успішність: ", st1.progress, st1.lvlife, "Настрій: ", st1.mood)
-print("\033[33mСтуденське життя:", st1.name, "\n\033[0m") #30-37 цвет текста, 40-47 цвет фона
-for i in range(1, 31):
-    if st1.lvlife==False:
+    def decide_action(self):
+        if self.money < 20:
+            return self.work
+        elif self.progress < 10:
+            return self.study
+        elif self.mood < 30:
+            return self.rest
+        else:
+            return random.choice([self.study, self.rest, self.sleep])
+
+    def lifeStud(self, day):
+        day = "\033[34mдень №" + str(day) + "\033[0m"
+        print(day)
+        action = self.decide_action()
+        action()
+        self.everyday()
+        self.isLvlife()
+
+st1 = Student("Сашко")
+print("\033[33mСтуденське життя:", st1.name, "\n\033[0m")
+for i in range(1, 366):
+    if not st1.lvlife:
         break
     st1.lifeStud(i)
 
-st2=Student("Маринки")
-#print(st1.name, "Успішність: ", st1.progress, st1.lvlife, "Настрій: ", st1.mood)
-print("\n\n\033[33mСтуденське життя:", st2.name, "\n\033[0m") #30-37 цвет текста, 40-47 цвет фона
-for i in range(1, 31):
-    if st2.lvlife==False:
+st2 = Student("Маринка")
+print("\n\n\033[33mСтуденське життя:", st2.name, "\n\033[0m")
+for i in range(1, 366):
+    if not st2.lvlife:
         break
     st2.lifeStud(i)
